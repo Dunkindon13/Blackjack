6bec4de2eb99e52e212225feaaa9e1be5ef39e62
Dunkindon13 <kingdonath@gmail.com>
2019-02-17 15:08:18 -0500

    Add files via upload

--- Blackjack/main.js
+++ Blackjack/main.js
@@ -4,22 +4,24 @@ var values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
 var deck = new Array();
 var players = new Array();
 var currentPlayer = 0;
-let balance = 100;
-let count = 0; //Card counter
-let bet = document.getElementById('betNum').value;
+var balance = 100;
+var count = 0; //Card counter
+var bet = document.getElementById('betNum').value;
+var message = document.getElementById("message");
 
 function createDeck()
 {
 	deck = new Array();
-	for (let i = 0 ; i < values.length; i++)
+	for (var i = 0 ; i < values.length; i++)
 	{
-		for(let x = 0; x < suits.length; x++)
+		for(var x = 0; x < suits.length; x++)
 		{
 			var weight = parseInt(values[i]);
 			if (values[i] == "J" || values[i] == "Q" || values[i] == "K")
 				weight = 10;
-			if (values[i] == "A")
-				weight = 11;
+			if (values[i] == "A"){
+				weight = 1;
+			}
 			var card = { Value: values[i], Suit: suits[x], Weight: weight };
 			deck.push(card);
 		}
@@ -30,18 +32,19 @@ function createPlayers(numPlayers)
 {
 	numPlayers = document.getElementById('numPlayers').value;
 	players = new Array();
-	for(let i = 1; i <= numPlayers; i++)
+	for(var i = 1; i <= numPlayers; i++)
 	{
 		var hand = new Array();
 		var player = { Name: 'Player ' + i, ID: i, Points: 0, Hand: hand };
 		players.push(player);
 	}
+	return numPlayers;
 }
 
 function createPlayersUI()
 {
 	document.getElementById('players').innerHTML = '';
-	for(var i = 0; i < players.length; i++)
+	for(let i = 0; i < players.length; i++)
 	{
 		var div_player = document.createElement('div');
 		var div_playerid = document.createElement('div');
@@ -66,7 +69,7 @@ function shuffle()
 {
 	// For 1000 turns
 	// Switch the values of two random cards
-	for (var i = 0; i < 1000; i++)
+	for (let i = 0; i < 1000; i++)
 	{
 		var location1 = Math.floor((Math.random() * deck.length));
 		var location2 = Math.floor((Math.random() * deck.length));
@@ -77,8 +80,11 @@ function shuffle()
 	}
 }
 
-function startblackjack()
+function startblackjack(numPlayers)
 {
+	if (numPlayers < 1) {
+		message.innerHTML = "Number of players must have a value of at least 1.<br>Press Start to begin again.";
+	}
 	document.getElementById('btnStart').value = 'Restart';
 	document.getElementById("status").style.display="none";
 	// Deal 2 cards to every player object
@@ -108,7 +114,7 @@ function dealHands()
 		}
 	}
 	updateDeck();
-	
+
 }
 
 function renderCard(card, player)
@@ -153,6 +159,12 @@ function updatePoints()
 	{
 		getPoints(i);
 		document.getElementById('points_' + i).innerHTML = players[i].Points;
+		/*if (players[i].Points > 21 && )
+		{
+			weight = 1;
+		}else{
+			weight = 11;
+		}*/
 	}
 }
 
@@ -160,6 +172,7 @@ function hitMe()
 {
 	//Gives a card from the deck to the current player
 	//Check if current player is over 21
+	document.getElementById('stayBtn').value = 'Stay';
 	var card = deck.pop();
 	players[currentPlayer].Hand.push(card);
 	renderCard(card, currentPlayer);
@@ -172,6 +185,7 @@ function hitMe()
 function stay()
 {
 	// Move on to next player, if any
+	document.getElementById('hitBtn').disabled = false;
 	if (currentPlayer != players.length-1) {
 		document.getElementById('player_' + currentPlayer).classList.remove('active');
 		currentPlayer += 1;
@@ -179,11 +193,11 @@ function stay()
 	}
 
 	else {
-		end();
+		endGame();
 	}
 }
 
-function end()
+function endGame()
 {
 	var winner = -1;
 	var score = 0;
@@ -206,9 +220,14 @@ function check()
 {
 	if (players[currentPlayer].Points > 21)
 	{
+		document.getElementById('stayBtn').value = 'Next Player';
 		document.getElementById('status').innerHTML = 'Player: ' + players[currentPlayer].ID + ' went bust!';
 		document.getElementById('status').style.display = "inline-block";
-		end();
+		document.getElementById('hitBtn').disabled = true;
+
+		if(players[currentPlayer] == players.length){
+			endGame();
+		}
 	}
 }
 
@@ -219,13 +238,13 @@ function updateDeck()
 
 function getBalance(player)
 {
-	//var points = 0;
+	let balance = document.getElementById('amountLeft').value;
 	for(let i = 0; i < players.length; i++)
 	{
-		balance += players[player].Hand[i].Weight;
+		balance = players[player].balance;
 	}
-	players[player].Points = points;
-	return points;
+	document.getElementById('amountLeft').value = balance;
+	return balance;
 }
 
 function updateBalance(balance, bet)
@@ -239,14 +258,14 @@ function updateBalance(balance, bet)
 		else{
 			document.getElementById('amountLeft').innerHTML = balance-bet;
 		}
-		
+
 		for (var i = 0 ; i < players.length; i++)
 		{
 			getPoints(i);
 			document.getElementById('points_' + i).innerHTML = players[i].Points;
 		}
 	}
-	
+
 }
 
 function updateCount(card, count)
